13ab4c52a2fa345288825c3f45a34d0ef9661cb6
SimoneLeveghi <simone.leveghi@buonarroti.tn.it>
2024-03-11 09:11:31 +0100

    Add boilerplate to Product

---
+++ src/Product.java
@@ -0,0 +1,31 @@
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) throws Exception {
+        setName(name);
+        setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        this.name = this.name.trim();
+        while(this.name.contains("  ")) {
+            this.name = this.name.replaceAll("  ", " ");
+        }
+        this.name = this.name.toLowerCase();
+    }
+
+    public void setPrice(double price) throws Exception {
+        if(price <= 0) throw new Exception("Invalid price");
+        this.price = price;
+    }
+}
